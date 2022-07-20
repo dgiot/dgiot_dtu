6b53bf4a38228a892c459e0c0ef5970125460566
Restyled.io <commits@restyled.io>
2022-07-20 09:54:49 +0000

    Restyled by clang-format

--- component/Mqtt/MqttClientHelper.cs
+++ component/Mqtt/MqttClientHelper.cs
@@ -2,8 +2,7 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
-namespace Dgiot_dtu
-{
+namespace Dgiot_dtu {
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -17,290 +16,254 @@ using MQTTnet.Core.Client;
 using MQTTnet.Core.Packets;
 using MQTTnet.Core.Protocol;
 
-public class MqttClientHelper
-{
+public class MqttClientHelper {
 
-    private MqttClientHelper()
-    {
+  private MqttClientHelper() {}
+
+  private static MqttClient mqttClient = null;
+  private static string server = "prod.iotn2n.com";
+  private static int port = 1883;
+  private static string subtopic = "thing/com/";
+  private static string pubtopic = "thing/com/post/";
+  private static string clientid = Guid.NewGuid().ToString().Substring(0, 5);
+  private static string username = "dgiot";
+  private static string password = "dgiot";
+  private static MqttClientHelper instance = null;
+  private static bool bIsRunning = false;
+  private static bool bIsCheck = false;
+  private static bool bAutoReconnect = false;
+  private static string dtuAddr = "";
+
+  public static MqttClientHelper GetInstance() {
+    if (instance == null) {
+      instance = new MqttClientHelper();
     }
 
-    private static MqttClient mqttClient = null;
-    private static string server = "prod.iotn2n.com";
-    private static int port = 1883;
-    private static string subtopic = "thing/com/";
-    private static string pubtopic = "thing/com/post/";
-    private static string clientid = Guid.NewGuid().ToString().Substring(0, 5);
-    private static string username = "dgiot";
-    private static string password = "dgiot";
-    private static MqttClientHelper instance = null;
-    private static bool bIsRunning = false;
-    private static bool bIsCheck = false;
-    private static bool bAutoReconnect = false;
-    private static string dtuAddr = "";
+    return instance;
+  }
 
-    public static MqttClientHelper GetInstance()
-    {
-        if (instance == null)
-        {
-            instance = new MqttClientHelper();
+  public static void Start() {
+    Config();
+    bIsRunning = true;
+    if (bIsCheck) {
+      Task.Run(async () => { await ConnectMqttServerAsync(); });
+    }
+  }
+
+  public static void Stop() {
+    if (mqttClient != null) {
+      bAutoReconnect = false;
+      bIsRunning = false;
+      Task.Run(async () => { await DisConnectMqttServerAsync(); });
+    }
+  }
+
+  public static void Config() {
+    bAutoReconnect =
+        DgiotHelper.StrTobool(ConfigHelper.GetConfig("ReconnectChecked"));
+    server = ConfigHelper.GetConfig("DgiotSever");
+    LogHelper.Log("DgiotPort " + ConfigHelper.GetConfig("DgiotPort"));
+    port = int.Parse(ConfigHelper.GetConfig("DgiotPort"));
+    clientid = ConfigHelper.GetConfig("MqttClientId");
+    username = ConfigHelper.GetConfig("MqttUserName");
+    password = ConfigHelper.GetConfig("MqttPassword");
+    pubtopic = ConfigHelper.GetConfig("MqttPubTopic");
+    subtopic = ConfigHelper.GetConfig("MqttSubTopic");
+    dtuAddr = ConfigHelper.GetConfig("DtuAddr");
+    bIsCheck =
+        DgiotHelper.StrTobool(ConfigHelper.GetConfig("MqttClient_Checked"));
+  }
+
+  public void Publish(byte[] payload) {
+    var appMsg = new MqttApplicationMessage(
+        pubtopic, payload, MqttQualityOfServiceLevel.AtLeastOnce, false);
+    mqttClient.PublishAsync(appMsg);
+  }
+
+  public static void Publish(string pubtopic, byte[] payload) {
+    if (mqttClient != null && mqttClient.IsConnected) {
+      var appMsg = new MqttApplicationMessage(
+          pubtopic, payload, MqttQualityOfServiceLevel.AtLeastOnce, false);
+      mqttClient.PublishAsync(appMsg);
+    }
+  }
+
+  private static async Task ReConnectMqttServerAsync() {
+    while (bIsRunning) {
+      if (!bAutoReconnect) {
+        break;
+      }
+
+      Thread.Sleep(1000 * 10);
+      if (!mqttClient.IsConnected) {
+        await ConnectMqttServerAsync();
+      }
+    }
+  }
+
+  private static async Task ConnectMqttServerAsync() {
+    if (mqttClient == null) {
+      mqttClient = new MqttClientFactory().CreateMqttClient() as MqttClient;
+      mqttClient.ApplicationMessageReceived +=
+          MqttClient_ApplicationMessageReceived;
+      mqttClient.Connected += MqttClient_Connected;
+      mqttClient.Disconnected += MqttClient_Disconnected;
+    }
+
+    try {
+      var options =
+          new MqttClientTcpOptions { Server = server,     ClientId = clientid,
+                                     UserName = username, Password = password,
+                                     Port = port,         CleanSession = true };
+
+      await DisConnectMqttServerAsync();
+      await mqttClient.ConnectAsync(options);
+      await ReConnectMqttServerAsync();
+    } catch (Exception ex) {
+      LogHelper.Log(ex.ToString());
+    }
+  }
+
+  private static async Task DisConnectMqttServerAsync() {
+    try {
+      await mqttClient.DisconnectAsync();
+    } catch (Exception ex) {
+      LogHelper.Log(ex.ToString());
+    }
+  }
+
+  /// <summary>
+  /// 服务器连接成功
+  /// </summary>
+  /// <param name="sender"></param>
+  /// <param name="e"></param>
+  private static void MqttClient_Connected(object sender, EventArgs e) {
+    LogHelper.Log("mqtt client:" + clientid + " connected");
+
+    mqttClient.SubscribeAsync(
+        new TopicFilter("/" + username + "/" + dtuAddr + "/device/#",
+                        MqttQualityOfServiceLevel.AtLeastOnce));
+
+    PrinterHelper.Start(mqttClient);
+  }
+
+  /// <summary>
+  /// 断开服务器连接
+  /// </summary>
+  /// <param name="sender"></param>
+  /// <param name="e"></param>
+  private static void MqttClient_Disconnected(object sender, EventArgs e) {
+    if (bAutoReconnect) {
+      _ = ReConnectMqttServerAsync();
+    } else {
+      LogHelper.Log("mqtt:" + clientid + " disconnected");
+    }
+  }
+
+  /// <summary>
+  /// 接收到消息
+  /// </summary>
+  /// <param name="sender"></param>
+  /// <param name="e"></param>
+  private static void MqttClient_ApplicationMessageReceived(
+      object sender, MqttApplicationMessageReceivedEventArgs e) {
+    Dictionary<string, object> json = Get_payload(e.ApplicationMessage.Payload);
+    string topic = e.ApplicationMessage.Topic;
+    string data = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+    //   LogHelper.Log("mqtt:topic: "+ topic);
+    //   foreach (var item in json)
+    //  {
+    //      LogHelper.Log("mqtt:json: " + " key :" + item.Key + " value " +
+    //      item.Value.ToString());
+    //   }
+
+    Regex r_subtopic = new Regex(subtopic); // 定义一个Regex对象实例
+    Match m_subtopic =
+        r_subtopic.Match(e.ApplicationMessage.Topic); // 在字符串中匹配
+    if (m_subtopic.Success) {
+      SerialPortHelper.Write(e.ApplicationMessage.Payload, 0,
+                             e.ApplicationMessage.Payload.Length);
+    }
+
+    if (topic.IndexOf("/" + username + "/" + dtuAddr + "/device/event") == 0) {
+      if (json.ContainsKey("cmd")) {
+        if (json["cmd"].ToString() == "opc_items") {
+          OPCDAHelper.Additems(json);
+        } else if (json["cmd"].ToString() == "opc_report") {
+          OPCDAHelper.Publishvalues(json);
         }
-
-        return instance;
+      }
     }
-
-    public static void Start()
-    {
-        Config();
-        bIsRunning = true;
-        if (bIsCheck)
+    if (topic.IndexOf("device/printer") == 0) {
+      if (json.ContainsKey("barcode")) {
+        if (json["DtuNum"].ToString() == "1") //判断电脑编号进行打印
         {
-            Task.Run(async () => {
-                await ConnectMqttServerAsync();
-            });
+          Dgiot_dtu.PrinterHelper.PrintPage(json["barcode"].ToString());
         }
+      }
     }
 
-    public static void Stop()
-    {
-        if (mqttClient != null)
-        {
-            bAutoReconnect = false;
-            bIsRunning = false;
-            Task.Run(async () => {
-                await DisConnectMqttServerAsync();
-            });
-        }
+    AccessHelper.Do_mdb(topic, json, clientid);
+
+    MqttServerHelper.Write(e.ApplicationMessage);
+  }
+
+  public static void Write(byte[] data, int offset, int len) {
+    if (bIsCheck) {
+      var appMsg = new MqttApplicationMessage(
+          pubtopic + clientid,
+          Encoding.UTF8.GetBytes(LogHelper.Logdata(data, offset, len)),
+          MqttQualityOfServiceLevel.AtLeastOnce, false);
+      LogHelper.Log("mqtt client publish:" +
+                    LogHelper.Logdata(data, offset, len));
+      mqttClient.PublishAsync(appMsg);
     }
+  }
 
-    public static void Config()
-    {
-        bAutoReconnect = DgiotHelper.StrTobool(ConfigHelper.GetConfig("ReconnectChecked"));
-        server = ConfigHelper.GetConfig("DgiotSever");
-        LogHelper.Log("DgiotPort " + ConfigHelper.GetConfig("DgiotPort"));
-        port = int.Parse(ConfigHelper.GetConfig("DgiotPort"));
-        clientid = ConfigHelper.GetConfig("MqttClientId");
-        username = ConfigHelper.GetConfig("MqttUserName");
-        password = ConfigHelper.GetConfig("MqttPassword");
-        pubtopic = ConfigHelper.GetConfig("MqttPubTopic");
-        subtopic = ConfigHelper.GetConfig("MqttSubTopic");
-        dtuAddr = ConfigHelper.GetConfig("DtuAddr");
-        bIsCheck = DgiotHelper.StrTobool(ConfigHelper.GetConfig("MqttClient_Checked"));
+  public static void Write(MqttApplicationMessage appMsg) {
+    if (bIsCheck) {
+      mqttClient.PublishAsync(appMsg);
     }
+  }
 
-    public void Publish(byte[] payload)
-    {
-        var appMsg = new MqttApplicationMessage(pubtopic, payload, MqttQualityOfServiceLevel.AtLeastOnce, false);
-        mqttClient.PublishAsync(appMsg);
-    }
+  private static Dictionary<string, object> Get_payload(byte[] payload) {
+    string data = Encoding.UTF8.GetString(payload);
+    JavaScriptSerializer serializer = new JavaScriptSerializer();
+    Dictionary<string, object> json =
+        (Dictionary<string, object>)serializer.DeserializeObject(data);
+    return json;
+  }
 
-    public static void Publish(string pubtopic, byte[] payload)
-    {
-        if (mqttClient != null && mqttClient.IsConnected)
-        {
-            var appMsg = new MqttApplicationMessage(pubtopic, payload, MqttQualityOfServiceLevel.AtLeastOnce, false);
-            mqttClient.PublishAsync(appMsg);
-        }
-    }
+  private static Dictionary<string, object> Get_payload(byte[] payload,
+                                                        int offset, int len) {
+    string data = Encoding.UTF8.GetString(payload, offset, len);
+    JavaScriptSerializer serializer = new JavaScriptSerializer();
+    Dictionary<string, object> json =
+        (Dictionary<string, object>)serializer.DeserializeObject(data);
+    return json;
+  }
 
-    private static async Task ReConnectMqttServerAsync()
-    {
-        while (bIsRunning)
-        {
-            if (!bAutoReconnect)
-            {
-                break;
-            }
+  private static readonly DateTime BaseTime = new DateTime(1970, 1, 1);
 
-            Thread.Sleep(1000 * 10);
-            if (!mqttClient.IsConnected)
-            {
-                await ConnectMqttServerAsync();
-            }
-        }
-    }
+  /// <summary>
+  /// 将unixtime转换为.NET的DateTime
+  /// </summary>
+  /// <param name="timeStamp">秒数</param>
+  /// <returns>转换后的时间</returns>
+  public static DateTime FromUnixTime(long timeStamp) {
+    return TimeZone.CurrentTimeZone.ToLocalTime(
+        new DateTime((timeStamp * 10000000) + BaseTime.Ticks));
+  }
 
-    private static async Task ConnectMqttServerAsync()
-    {
-        if (mqttClient == null)
-        {
-            mqttClient = new MqttClientFactory().CreateMqttClient() as MqttClient;
-            mqttClient.ApplicationMessageReceived += MqttClient_ApplicationMessageReceived;
-            mqttClient.Connected += MqttClient_Connected;
-            mqttClient.Disconnected += MqttClient_Disconnected;
-        }
-
-        try
-        {
-            var options = new MqttClientTcpOptions
-            {
-                Server = server,
-                ClientId = clientid,
-                UserName = username,
-                Password = password,
-                Port = port,
-                CleanSession = true
-            };
-
-            await DisConnectMqttServerAsync();
-            await mqttClient.ConnectAsync(options);
-            await ReConnectMqttServerAsync();
-        }
-        catch (Exception ex)
-        {
-            LogHelper.Log(ex.ToString());
-        }
-    }
-
-    private static async Task DisConnectMqttServerAsync()
-    {
-        try
-        {
-            await mqttClient.DisconnectAsync();
-        }
-        catch (Exception ex)
-        {
-            LogHelper.Log(ex.ToString());
-        }
-    }
-
-    /// <summary>
-    /// 服务器连接成功
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    private static void MqttClient_Connected(object sender, EventArgs e)
-    {
-        LogHelper.Log("mqtt client:" + clientid + " connected");
-
-        mqttClient.SubscribeAsync(new TopicFilter("/" + username + "/" + dtuAddr + "/device/#", MqttQualityOfServiceLevel.AtLeastOnce));
-
-        PrinterHelper.Start(mqttClient);
-    }
-
-    /// <summary>
-    /// 断开服务器连接
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    private static void MqttClient_Disconnected(object sender, EventArgs e)
-    {
-        if (bAutoReconnect)
-        {
-            _ = ReConnectMqttServerAsync();
-        }
-        else
-        {
-            LogHelper.Log("mqtt:" + clientid + " disconnected");
-        }
-    }
-
-    /// <summary>
-    /// 接收到消息
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    private static void MqttClient_ApplicationMessageReceived(object sender, MqttApplicationMessageReceivedEventArgs e)
-    {
-        Dictionary<string, object> json = Get_payload(e.ApplicationMessage.Payload);
-        string topic = e.ApplicationMessage.Topic;
-        string data = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
-        //   LogHelper.Log("mqtt:topic: "+ topic);
-        //   foreach (var item in json)
-        //  {
-        //      LogHelper.Log("mqtt:json: " + " key :" + item.Key + " value " +  item.Value.ToString());
-        //   }
-
-        Regex r_subtopic = new Regex(subtopic); // 定义一个Regex对象实例
-        Match m_subtopic = r_subtopic.Match(e.ApplicationMessage.Topic); // 在字符串中匹配
-        if (m_subtopic.Success)
-        {
-            SerialPortHelper.Write(e.ApplicationMessage.Payload, 0, e.ApplicationMessage.Payload.Length);
-        }
-
-        if (topic.IndexOf("/" + username + "/" + dtuAddr + "/device/event") == 0)
-        {
-            if (json.ContainsKey("cmd"))
-            {
-                if (json["cmd"].ToString() == "opc_items")
-                {
-                    OPCDAHelper.Additems(json);
-                }
-                else if (json["cmd"].ToString() == "opc_report")
-                {
-                    OPCDAHelper.Publishvalues(json);
-                }
-            }
-        }
-        if (topic.IndexOf("device/printer") == 0)
-        {
-            if (json.ContainsKey("barcode"))
-            {
-                if (json["DtuNum"].ToString() == "1")      //判断电脑编号进行打印
-                {
-                    Dgiot_dtu.PrinterHelper.PrintPage(json["barcode"].ToString());
-                }
-            }
-        }
-
-        AccessHelper.Do_mdb(topic,  json, clientid);
-
-        MqttServerHelper.Write(e.ApplicationMessage);
-    }
-
-    public static void Write(byte[] data, int offset, int len)
-    {
-        if (bIsCheck)
-        {
-            var appMsg = new MqttApplicationMessage(pubtopic + clientid, Encoding.UTF8.GetBytes(LogHelper.Logdata(data, offset, len)), MqttQualityOfServiceLevel.AtLeastOnce, false);
-            LogHelper.Log("mqtt client publish:" + LogHelper.Logdata(data, offset, len));
-            mqttClient.PublishAsync(appMsg);
-        }
-    }
-
-    public static void Write(MqttApplicationMessage appMsg)
-    {
-        if (bIsCheck)
-        {
-            mqttClient.PublishAsync(appMsg);
-        }
-    }
-
-    private static Dictionary<string, object> Get_payload(byte[] payload)
-    {
-        string data = Encoding.UTF8.GetString(payload);
-        JavaScriptSerializer serializer = new JavaScriptSerializer();
-        Dictionary<string, object> json = (Dictionary<string, object>)serializer.DeserializeObject(data);
-        return json;
-    }
-
-    private static Dictionary<string, object> Get_payload(byte[] payload, int offset, int len)
-    {
-        string data = Encoding.UTF8.GetString(payload, offset, len);
-        JavaScriptSerializer serializer = new JavaScriptSerializer();
-        Dictionary<string, object> json = (Dictionary<string, object>)serializer.DeserializeObject(data);
-        return json;
-    }
-
-    private static readonly DateTime BaseTime = new DateTime(1970, 1, 1);
-
-    /// <summary>
-    /// 将unixtime转换为.NET的DateTime
-    /// </summary>
-    /// <param name="timeStamp">秒数</param>
-    /// <returns>转换后的时间</returns>
-    public static DateTime FromUnixTime(long timeStamp)
-    {
-        return TimeZone.CurrentTimeZone.ToLocalTime(new DateTime((timeStamp * 10000000) + BaseTime.Ticks));
-    }
-
-    /// <summary>
-    /// 将.NET的DateTime转换为unix time
-    /// </summary>
-    /// <param name="dateTime">待转换的时间</param>
-    /// <returns>转换后的unix time</returns>
-    public static long FromDateTime(DateTime dateTime)
-    {
-        return (TimeZone.CurrentTimeZone.ToUniversalTime(dateTime).Ticks - BaseTime.Ticks) / 10000000;
-    }
+  /// <summary>
+  /// 将.NET的DateTime转换为unix time
+  /// </summary>
+  /// <param name="dateTime">待转换的时间</param>
+  /// <returns>转换后的unix time</returns>
+  public static long FromDateTime(DateTime dateTime) {
+    return (TimeZone.CurrentTimeZone.ToUniversalTime(dateTime).Ticks -
+            BaseTime.Ticks) /
+           10000000;
+  }
 }
 }
--- component/Printer/PrinterHelper.cs
+++ component/Printer/PrinterHelper.cs
@@ -2,8 +2,7 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
-namespace Dgiot_dtu
-{
+namespace Dgiot_dtu {
 using System;
 using System.Configuration;
 using System.Drawing;
@@ -15,124 +14,97 @@ using MQTTnet.Core.Client;
 using MQTTnet.Core.Packets;
 using MQTTnet.Core.Protocol;
 
-internal class PrinterHelper
-{
+internal class PrinterHelper {
 
-    [DllImport("winspool.drv")]
-    public static extern bool SetDefaultPrinter(String Name); //调用win api将指定名称的打印机设置为默认打印
+  [DllImport("winspool.drv")]
+  public static extern bool
+  SetDefaultPrinter(String Name); //调用win api将指定名称的打印机设置为默认打印
 
-    private static PrinterHelper instance = null;
-    private static bool bIsRunning = false;
+  private static PrinterHelper instance = null;
+  private static bool bIsRunning = false;
 
-    private static string textData = "C01010100005DGF0100189";
-    private static PrintDocument fPrintDocument = new PrintDocument();
+  private static string textData = "C01010100005DGF0100189";
+  private static PrintDocument fPrintDocument = new PrintDocument();
 
+  public static void Start(MqttClient mqttClient) {
 
-    public static void Start(MqttClient mqttClient)
-    {
+    string Name = "ec71804a3d";
+    mqttClient.SubscribeAsync(new TopicFilter(
+        "device/printer", MqttQualityOfServiceLevel.AtLeastOnce));
+    LogHelper.Log("mqtt client subscribe topic: " + "device/" + Name + "/#");
+  }
 
-        string Name = "ec71804a3d";
-        mqttClient.SubscribeAsync(new TopicFilter("device/printer", MqttQualityOfServiceLevel.AtLeastOnce));
-        LogHelper.Log("mqtt client subscribe topic: " + "device/" + Name + "/#");
+  public static PrinterHelper GetInstance() {
+    if (instance == null) {
+      instance = new PrinterHelper();
     }
 
+    return instance;
+  }
 
+  public static void Start(KeyValueConfigurationCollection config,
+                           bool bIsRunning) {
+    Config(config);
+    PrinterHelper.bIsRunning = bIsRunning;
+  }
 
-    public static PrinterHelper GetInstance()
-    {
-        if (instance == null)
-        {
-            instance = new PrinterHelper();
-        }
+  public static void Stop() { PrinterHelper.bIsRunning = false; }
 
-        return instance;
-    }
+  public static void Config(KeyValueConfigurationCollection config) {}
 
-    public static void Start(KeyValueConfigurationCollection config, bool bIsRunning)
-    {
-        Config(config);
-        PrinterHelper.bIsRunning = bIsRunning;
-    }
+  Brush brushHeaderFont = new SolidBrush(Color.Black);
+  Pen LinePen = new Pen(Color.Black, 1);
+  public PrinterHelper() { InitPrint(); }
+  /// <summary>
+  /// 初始化打印机
+  /// </summary>
+  private void InitPrint() {}
 
-    public static void Stop()
-    {
-        PrinterHelper.bIsRunning = false;
-    }
+  /// <summary>
+  /// 打印
+  /// </summary>
+  public void Print() {}
 
-    public static void Config(KeyValueConfigurationCollection config)
-    {
-    }
+  public static void SetTextData(string data) { textData = data; }
 
+  public static void PrintPage(string data) {
+    SetTextData(data);
+    PrintDialog PD = new PrintDialog();
+    PageSettings pageSettings = new PageSettings();
+    // pageSettings.PaperSize = new PaperSize("Size", 30, 40);
+    string Name = fPrintDocument.PrinterSettings.PrinterName;
+    System.Drawing.Printing.PrinterSettings Ps =
+        new System.Drawing.Printing.PrinterSettings();
+    // SetDefaultPrinter("Deli DL-888B(NEW)");
+    fPrintDocument.DefaultPageSettings.PaperSize =
+        new PaperSize("Size", 157, 118); //单位1/100英寸
+    SetDefaultPrinter(Name);
+    PD.PrinterSettings = Ps;
+    fPrintDocument.PrintPage += document_PrintPage;
+    PD.Document = fPrintDocument;
+    fPrintDocument.Print();
+  }
 
+  static void document_PrintPage(object sender, PrintPageEventArgs e) {
+    int x1 = -5;
+    int y1 = 15;
+    double x2 = (260 * 0.63) + x1;
+    double y2 = (140 * 0.63) + y1;
+    e.Graphics.DrawImage(PrinterHelper.Get_image(), x1, y1, (int)x2, (int)y2);
+    e.HasMorePages = false;
+  }
 
-
-    Brush brushHeaderFont = new SolidBrush(Color.Black);
-    Pen LinePen = new Pen(Color.Black, 1);
-    public PrinterHelper()
-    {
-        InitPrint();
-    }
-    /// <summary>
-    /// 初始化打印机
-    /// </summary>
-    private void InitPrint()
-    {
-
-    }
-
-    /// <summary>
-    /// 打印
-    /// </summary>
-    public void Print()
-    {
-    }
-
-    public static void SetTextData(string data )
-    {
-        textData = data;
-
-    }
-
-
-    public static void PrintPage(string data)
-    {
-        SetTextData(data);
-        PrintDialog PD = new PrintDialog();
-        PageSettings pageSettings = new PageSettings();
-        //pageSettings.PaperSize = new PaperSize("Size", 30, 40);
-        string Name = fPrintDocument.PrinterSettings.PrinterName;
-        System.Drawing.Printing.PrinterSettings Ps = new System.Drawing.Printing.PrinterSettings();
-        //SetDefaultPrinter("Deli DL-888B(NEW)");
-        fPrintDocument.DefaultPageSettings.PaperSize = new PaperSize("Size", 157, 118); //单位1/100英寸
-        SetDefaultPrinter(Name);
-        PD.PrinterSettings = Ps;
-        fPrintDocument.PrintPage += document_PrintPage;
-        PD.Document = fPrintDocument;
-        fPrintDocument.Print();
-    }
-
-    static void document_PrintPage(object sender, PrintPageEventArgs e)
-    {
-        int x1 = -5;
-        int y1 = 15;
-        double x2= (260 * 0.63)+x1;
-        double y2 = (140 * 0.63) + y1;
-        e.Graphics.DrawImage(PrinterHelper.Get_image(), x1, y1,(int)x2, (int)y2);
-        e.HasMorePages = false;
-    }
-
-    public static Image Get_image()
-    {
-        CodeLib.Barcode b = new CodeLib.Barcode();
-        CodeLib.TYPE type = CodeLib.TYPE.CODE128B;
-        b.IncludeLabel = true;
-        string familyName = "宋体";
-        Font Label  = new Font(familyName, 40.0F);
-        b.LabelFont = Label;
-        CodeLib.AlignmentPositions Align = CodeLib.AlignmentPositions.CENTER;
-        b.Alignment = Align;
-        b.LabelPosition = CodeLib.LabelPositions.BOTTOMCENTER;
-        return b.Encode(type, textData, 523, 200);
-    }
+  public static Image Get_image() {
+    CodeLib.Barcode b = new CodeLib.Barcode();
+    CodeLib.TYPE type = CodeLib.TYPE.CODE128B;
+    b.IncludeLabel = true;
+    string familyName = "宋体";
+    Font Label = new Font(familyName, 40.0F);
+    b.LabelFont = Label;
+    CodeLib.AlignmentPositions Align = CodeLib.AlignmentPositions.CENTER;
+    b.Alignment = Align;
+    b.LabelPosition = CodeLib.LabelPositions.BOTTOMCENTER;
+    return b.Encode(type, textData, 523, 200);
+  }
 }
 }
